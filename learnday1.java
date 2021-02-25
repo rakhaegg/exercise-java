c73a806ad3714922b02b6718fe2a233881dea48c
rakhaegg <rakhae.g@gmail.com>
2021-02-25 12:20:08 +0700

    rasa

--- learnday1.java
+++ learnday1.java
@@ -5,12 +5,23 @@ import java.io.BufferedWriter;
 public class learnday1{
 
     public static void main(String[] args) {
-       
-        System.out.print( Boolean.parseBoolean("true"));
-        System.out.print(Boolean.valueOf("true"));
-        System.out.print(Boolean.toString("true"));
-        System.out.print("Rakha".toString());
-        System.out.print(1.hashCode());
+        
+        System.out.println(modifierbooleanValue());
     }
+
+    public static Boolean modifierParseBoolean() {
+        String name = "Hello World";
+        Boolean whatIs = true;
+
+        Boolean answer = whatIs.parseBoolean(name);
+
+        return answer;
+    }
+    public static boolean modifierbooleanValue(){
+        Boolean answer = true;
+        
+        return answer.booleanValue();
+    }
+    public s
 
 }
