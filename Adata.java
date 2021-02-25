7813129f46d35178edf3075ddeb926fb96f8d2df
rakhaegg <rakhae.g@gmail.com>
2021-02-25 14:03:10 +0700

    master

---
+++ Adata.java
@@ -0,0 +1,10 @@
+
+abstract class Adata {
+    public abstract void print();
+
+    public void printName(){
+        System.out.println("Nama : ");
+    }
+
+}
+
---
+++ Adata_data1.java
@@ -0,0 +1,11 @@
+
+public class Adata_data1 extends Adata {
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+
+    }
+
+    
+}
