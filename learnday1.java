3dab148e44103fb5714bc788cb09709825062801
rakhaegg <rakhae.g@gmail.com>
2021-02-25 12:03:46 +0700

    first

---
+++ learnday1.java
@@ -0,0 +1,16 @@
+
+import java.io.BufferedWriter;
+
+
+public class learnday1{
+
+    public static void main(String[] args) {
+       
+        System.out.print( Boolean.parseBoolean("true"));
+        System.out.print(Boolean.valueOf("true"));
+        System.out.print(Boolean.toString("true"));
+        System.out.print("Rakha".toString());
+        System.out.print(1.hashCode());
+    }
+
+}
